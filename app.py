dcc677f72f78436200ea75c034106c1171520823
Choy Wei Qiang <73052459+choyweiqiang@users.noreply.github.com>
2022-11-03 11:21:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/cs440'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/cs440'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-CORS(app)  
+CORS(app)
 
-class status(db.Model):
+
+class Status(db.Model):
     __tablename__ = 'ransomware'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -24,10 +25,9 @@ class status(db.Model):
         return {"id": self.id, "status": self.status}
 
 
-
-@app.route("/getstatus")
-def getstatus(id):
-    status = status.query.filter_by(id=id).first()
+@app.route("/get_status/<int:id>")
+def getStatus(id):
+    status = Status.query.filter_by(id=id).first()
     if status:
         return jsonify(
             {
@@ -43,9 +43,10 @@ def getstatus(id):
     ), 404
 
 
-@app.route("/addstatus/<int:id>", methods=['POST'])
-def addstatus(id):
-    if (status.query.filter_by(id=id).first()):
+@app.route("/add_status/<int:id>", methods=['POST'])
+def addStatus(id):
+    status = Status.query.filter_by(id=id).first()
+    if (status):
         return jsonify(
             {
                 "code": 400,
@@ -56,8 +57,7 @@ def addstatus(id):
             }
         ), 400
 
-    data = request.get_json()
-    status = status(id, **data)
+    status = Status(id=id, status=0)
 
     try:
         db.session.add(status)
@@ -81,6 +81,42 @@ def addstatus(id):
     ), 201
 
 
+@app.route("/change_status/<int:id>", methods=['PUT'])
+def changeStatus(id):
+    status = Status.query.filter_by(id=id).first()
+    if not (status):
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "id": id
+                },
+                "message": "status doesn't exist"
+            }
+        ), 400
+
+    # status = Status(id=id, status=1)
+
+    try:
+        db.session.query(Status).update({Status.status: 1})
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "id": id
+                },
+                "message": "An error occurred status."
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": status.json()
+        }
+    ), 201
 
 
 if __name__ == '__main__':
